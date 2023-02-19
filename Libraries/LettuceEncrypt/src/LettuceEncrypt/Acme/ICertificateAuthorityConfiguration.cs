2ff925b9331d1dc41fa726a2ecfba6f67b47c876
Luc Vervoort <luc.vervoort@hogent.be>
2023-02-19 18:36:28 +0100

    Lettuce

---
+++ Libraries/LettuceEncrypt/src/LettuceEncrypt/Acme/ICertificateAuthorityConfiguration.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace LettuceEncrypt.Acme;
+
+/// <summary>
+/// Provides configuration for the certificate authority which implements the ACME protocol.
+/// </summary>
+public interface ICertificateAuthorityConfiguration
+{
+    /// <summary>
+    /// The base uri of the ACME protocol.
+    /// </summary>
+    Uri AcmeDirectoryUri { get; }
+}
